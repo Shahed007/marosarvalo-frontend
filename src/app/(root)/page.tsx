34c6d7db3773babba194d1f10bf8b56a2c81dbd8
sajib689 <sajibbabu751@gmail.com>
2025-09-03 08:03:01 +0600

    update

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,17 @@
+import AboutUs from "@/components/pages/home/AboutUs";
+import FAQ from "@/components/pages/home/FAQ";
+import GetInTouch from "@/components/pages/home/GetInTouch";
+import Hero from "@/components/pages/home/Hero";
+import MembershipPlans from "@/components/pages/home/MembershipPlans";
 
 export default function page() {
   return (
     <div>
-      
+      <Hero />
+      <AboutUs />
+      <MembershipPlans />
+      <FAQ />
+      <GetInTouch />
     </div>
-  )
+  );
 }
