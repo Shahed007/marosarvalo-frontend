a088ce3495c548ba22e1934c21d7b27dba828162
Nahidul Islam Siam <siamnahidul093@gmail.com>
2025-10-10 07:29:58 +0600

    add subscription

---
+++ src/app/(admin)/admin/subcription/add-subscription/page.tsx
@@ -0,0 +1,10 @@
+import SubscriptionPage from '@/components/admin/subscriptions/add-subscriptions'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <SubscriptionPage/>
+    </div>
+  )
+}
